d99aa454b8682b3bbe9e8a8c33cd31ba259c792a
ayauper <ayaup@qq.com>
2018-05-31 01:43:51 -0500

    Rate limit 3.5

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,21 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
+
+  $api->group([
+        'middleware' => 'api.throttle',
+        'limit' =>config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+
+
+
+  ], function($api) {
     // 短信验证码
     $api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
     $api->post('users', 'UsersController@store')
             ->name('api.users.store');
 
-
+});
 
 });
