2cd3fb103c1edb5d4f835e5d2d1c99c51efaaedf
ayauper <ayaup@qq.com>
2018-05-30 23:05:38 -0500

    verificationCode store 3.3

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Dingo\Api\Routing\Helpers;
+
 use App\Http\Controllers\Controller as BaseController;
 
 class Controller extends BaseController
