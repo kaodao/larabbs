09fbdc359c438c9c6ac17df53086d7ae3252ff9f
ayauper <ayaup@qq.com>
2018-05-31 14:44:31 -0500

    api/user

--- routes/api.php
+++ routes/api.php
@@ -16,15 +16,14 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
+    'namespace' => 'App\Http\Controllers\Api',
+     'middleware' => 'serializer:array'
 ], function($api) {
 
   $api->group([
         'middleware' => 'api.throttle',
         'limit' =>config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-
-
 
   ], function($api) {
     // 短信验证码
@@ -52,3 +51,18 @@ $api->delete('authorizations/current', 'AuthorizationsController@destroy')
 });
 
 });
+
+$api->group([
+       'middleware' => 'api.throttle',
+       'limit' => config('api.rate_limits.access.limit'),
+       'expires' => config('api.rate_limits.access.expires'),
+   ], function ($api) {
+       // 游客可以访问的接口
+
+       // 需要 token 验证的接口
+       $api->group(['middleware' => 'api.auth'], function($api) {
+           // 当前登录用户信息
+           $api->get('user', 'UsersController@me')
+               ->name('api.user.show');
+       });
+   });
